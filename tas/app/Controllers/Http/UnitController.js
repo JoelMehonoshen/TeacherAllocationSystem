e2e96f30b41a01a882784a38074ab4430a533f80
Jacob <n10473025@qut.edu.au>
2021-05-20 16:46:41 +1000

    controllers re-added and routing fixed - slight addition to db

--- tas/app/Controllers/Http/UnitController.js
+++ tas/app/Controllers/Http/UnitController.js
@@ -2,19 +2,10 @@
 
 class UnitController {
     async addunit({request, auth, response}) {
-        // const unit = new unit()
-        // academic.name = "John"
-        // academic.school = "Maths"
-        console.log("success")
-        // await academic.save()
-        // return response.redirect('/academics', true)
     }
 
     async render({request, response, view}) {
-        // //logic goes here
-        // const data = await Academic.all()
-
-        // return view.render('academics', {selectResponse: data.rows[0].school})
+        return view.render('units')
     }
 }
 
