3bb9a3c28bcedc3b998bbfe3ea639ec251d44d24
JessWill <jessica.williams0@protonmail.com>
2021-08-08 03:10:17 +0000

    allocations page

--- tas/app/Controllers/Http/AllocationController.js
+++ tas/app/Controllers/Http/AllocationController.js
@@ -1,15 +1,43 @@
 'use strict'
 
-const Allocation = use("App/Models/Allocation");
-const Academic = use("App/Models/Academic");
+const Database = use('Database')
 
 class AllocationController {
-    async render ({ view }) {
-        const allocations = await Allocation.all()
-        const academics = await Academic.all()
 
-        return view.render('allocations', { allocations: allocations.toJSON(), academics: academics.toJSON()})
+  async render({ view }) {
+    const academics = await Database.select('id', 'name','load').from('academics').distinct('id')
+    const allocations = await Database
+      .from('academics')
+      .select('academics.id', 'allocations.unit_code', 'allocations.load')
+      .join('allocations', 'academics.id', '=', 'allocations.id')
+
+    var allocAcademics = []
+    for (var i = 0; i < academics.length; i++) {
+      var teacher = {
+        id: academics[i].id,
+        name: academics[i].name,
+        requestedLoad: academics[i].load,
       }
+      var units = []
+      var totalLoad = 0;
+      for (var j = 0; j < allocations.length; j++) {
+        if (teacher.id == allocations[j].id) {
+          units.push(
+            {
+              unit_code: allocations[j].unit_code,
+              load: allocations[j].load
+            }
+          )
+          totalLoad += parseInt(allocations[j].load)
+        }
+      }
+      teacher.actualLoad = totalLoad
+      teacher.allocUnits = units
+      allocAcademics.push(teacher)
+    }
+    console.log(JSON.stringify(allocAcademics))
+    return view.render('allocations', { allocAcademics: allocAcademics })
+  }
 }
 
 module.exports = AllocationController
