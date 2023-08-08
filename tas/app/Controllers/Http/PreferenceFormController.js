5108d334794405e3e593024dcbd72704aca63fcf
Benjamin Dundon <benjamin.dundon@protonmail.com>
2023-08-08 15:05:30 +1000

    feature(PreferenceFormController): Added Controller
    Skeleton controller added for the Preference Form
    
    Signed-off-by: Benjamin Dundon <benjamin.dundon@protonmail.com>

---
+++ tas/app/Controllers/Http/PreferenceFormController.js
@@ -0,0 +1,12 @@
+"use strict"
+const Exception = use("App/Exceptions/Handler");
+const Logger = use("Logger");
+
+const Academic = use("App/Models/PreferenceForm");
+const Preference = use("App/Models/Academic");
+const Offering = use("App/Models/Offering")
+const Database = use("Database");
+
+class PreferenceFormController {
+    async offerings()
+}
