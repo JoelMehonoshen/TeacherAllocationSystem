2994a4b29d5bc8645b8fb5959edc66a523d225a3
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-08-13 22:21:26 +1000

    add a title and a selection button for sorting

--- tas/app/Controllers/Http/SpreadsheetViewController.js
+++ tas/app/Controllers/Http/SpreadsheetViewController.js
@@ -1,7 +1,6 @@
 "use strict";
 const Exception = use("App/Exceptions/Handler");
 const Logger = use("Logger");
-
 const Excel = require("exceljs");
 const Helpers = use("Helpers");
 const Academic = use("App/Models/Academic");
@@ -9,39 +8,35 @@ const Unit = use("App/Models/Unit");
 const Allocation = use("App/Models/Allocation");
 const Database = use("Database");
 
-
-
 class spreadsheetViewController {
   async render({ view, request }) {
-        try{
-          const academics = await Database.from("academics")
-          const units = await Database.from("units")
-          const allocations = await Database.from("allocations")
-          const offerings = await Database.from("offerings")
-          const preferences = await Database.from("preferences")
-          const global_spread = await Database
-          .from("academics")
-          .leftJoin("allocations",'academics.id','=','allocations.academicId')
-          .leftJoin("offerings",'allocations.id','=','offerings.id')
-          .select("academics.id AS academicId")
-          .select("allocations.fractionAllocated")
-          .select("offerings.code AS unitCode")
+    try{
+      const academics = await Database.from("academics")
+      const units = await Database.from("units")
+      const allocations = await Database.from("allocations")
+      const offerings = await Database.from("offerings")
+      const preferences = await Database.from("preferences")
+      const global_spread = await Database
+      .from("academics")
+      .leftJoin("allocations",'academics.id','=','allocations.academicId')
+      .leftJoin("offerings",'allocations.id','=','offerings.id')
+      .select("academics.id AS academicId")
+      .select("allocations.fractionAllocated")
+      .select("offerings.code AS unitCode")
 
-          return view.render("spreadsheetView", {
-            academics: academics,
-            units: units,
-            allocations: allocations,
-            offerings: offerings,
-            preferences: preferences,
-            global_spread:global_spread
-
-          });
-    
-        } catch (error) {
-          Logger.error(error);
-          throw new Exception();
-        }
-      }
+      return view.render("spreadsheetView", {
+        academics: academics,
+        units: units,
+        allocations: allocations,
+        offerings: offerings,
+        preferences: preferences,
+        global_spread: global_spread
+      });
+    } catch (error) {
+      Logger.error(error);
+      throw new Exception();
+    }
+  }
  }
 
 module.exports = spreadsheetViewController;
