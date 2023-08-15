2bfeb735f524df72202a3a29ead288fbfad4d472
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-08-15 20:51:10 +1000

    modify codes of the form submission in Academics table

--- tas/app/Controllers/Http/SpreadsheetViewController.js
+++ tas/app/Controllers/Http/SpreadsheetViewController.js
@@ -1,15 +1,10 @@
 "use strict";
 const Exception = use("App/Exceptions/Handler");
 const Logger = use("Logger");
-const Excel = require("exceljs");
-const Helpers = use("Helpers");
-const Academic = use("App/Models/Academic");
-const Unit = use("App/Models/Unit");
-const Allocation = use("App/Models/Allocation");
 const Database = use("Database");
 
 class SpreadsheetViewController {
-  async render({ view, request }) {
+  async render({ view }) {
     try{
       const academics = await Database.from("academics")
       const units = await Database.from("units")
@@ -17,12 +12,12 @@ class SpreadsheetViewController {
       const offerings = await Database.from("offerings")
       const preferences = await Database.from("preferences")
       const global_spread = await Database
-      .from("academics")
-      .leftJoin("allocations",'academics.id','=','allocations.academicId')
-      .leftJoin("offerings",'allocations.id','=','offerings.id')
-      .select("academics.id AS academicId")
-      .select("allocations.fractionAllocated")
-      .select("offerings.code AS unitCode")
+        .from("academics")
+        .leftJoin("allocations",'academics.id','=','allocations.academicId')
+        .leftJoin("offerings",'allocations.id','=','offerings.id')
+        .select("academics.id AS academicId")
+        .select("allocations.fractionAllocated")
+        .select("offerings.code AS unitCode")
 
       return view.render("spreadsheetView", {
         academics: academics,
