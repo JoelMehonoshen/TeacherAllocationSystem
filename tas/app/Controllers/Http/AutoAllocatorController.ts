86b7817991b996820dd1097e8246709d0b8a9a7f
Benjamin Dundon <benjamin.dundon@protonmail.com>
2023-09-03 16:48:32 +1000

    feature(AutoAllocationController): Add a controller for the automatic allocation functionality
    Currently this only contains a function to score an allocation based off an object containing raw data.
    
    This data contains self-scoring ints on willingness, expertise, prior years experience, and +ve/-ve student voice results
    
    Signed-off-by: Benjamin Dundon <benjamin.dundon@protonmail.com>

---
+++ tas/app/Controllers/Http/AutoAllocatorController.ts
@@ -0,0 +1,12 @@
+
+
+export default class AutoAllocatorController {
+
+    constructor(private willingWeight: number, private expertiseWeight: number, private priorWeight: number, private studVocSurvWeight: number ) {
+    } 
+
+    public scoreAllocation(allocationScores: any) {
+        return (this.willingWeight * allocationScores.willingness) + (this.expertiseWeight * allocationScores.expertise) 
+            + (this.priorWeight * allocationScores.priorYears) + (this.studVocSurvWeight * allocationScores.studentVoiceResult);
+    }
+}
