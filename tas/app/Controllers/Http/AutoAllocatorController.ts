212088ef03b0a5087785288af65da60afdf5d2a1
Benjamin Dundon <benjamin.dundon@protonmail.com>
2023-09-19 14:02:42 +1000

    feature(AutoAllocator): Account for Time
    The amount of time that an academic is available for, and the amount of time that a class requires, are now used to score.
    
    This weighting rewards having significantly more time than is required.
    
    Signed-off-by: Benjamin Dundon <benjamin.dundon@protonmail.com>

--- tas/app/Controllers/Http/AutoAllocatorController.ts
+++ tas/app/Controllers/Http/AutoAllocatorController.ts
@@ -2,11 +2,19 @@
 
 export default class AutoAllocatorController {
 
-    constructor(private willingWeight: number, private expertiseWeight: number, private priorWeight: number ) {
+    constructor( private willingWeight: number, private expertiseWeight: number, private priorWeight: number, private timeAvailable: number, private timeRequired: number ) {
     } 
 
+
+
     public scoreAllocation(allocationScores: any) {
-        return (this.willingWeight * allocationScores.willingness) + (this.expertiseWeight * allocationScores.expertise) 
-            + (this.priorWeight * allocationScores.priorYears);
+
+        // This multiplier will increase the total score if the person has more time available than the class requires, 
+        // and reduce it if they have less according to the magnitude of the difference.
+         const timeFitRatio: number = this.timeAvailable / this.timeRequired;
+
+        // Return the weighted sum of the factors scaled according to how well the academic's availability matches the class's requirements.
+        return ((this.willingWeight * allocationScores.willingness) + (this.expertiseWeight * allocationScores.expertise) 
+            + (this.priorWeight * allocationScores.priorYears)) * timeFitRatio;
     }
 }
