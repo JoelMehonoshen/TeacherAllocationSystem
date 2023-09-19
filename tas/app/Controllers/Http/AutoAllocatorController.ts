3cd2754ec74fe00de4ffe6ee6aa464dd9925a5f7
Benjamin Dundon <benjamin.dundon@protonmail.com>
2023-09-19 14:14:29 +1000

    refactor(AutoAllocation): Refactor scoreAllocation
    scoreAllocation now takes time as arguements, rather than making them classwide params.
    
    Signed-off-by: Benjamin Dundon <benjamin.dundon@protonmail.com>

--- tas/app/Controllers/Http/AutoAllocatorController.ts
+++ tas/app/Controllers/Http/AutoAllocatorController.ts
@@ -1,3 +1,5 @@
+import formResponses from "../../Models/formResponses";
+
 /**
  * Class to score an an allocation, and handle backend for the autoallocation.
  * Weights can be set in the ENV file, or manually when calling the class.
@@ -9,16 +11,21 @@
  */
 export default class AutoAllocatorController {
 
-    constructor( private willingWeight: number, private expertiseWeight: number, private priorWeight: number, private timeAvailable: number, private timeRequired: number ) {
+    constructor( private willingWeight: number, private expertiseWeight: number, private priorWeight: number ) {
     } 
 
-
-
-    public scoreAllocation(allocationScores: any) {
+    /**
+     * 
+     * @param allocationScores [formResponses] An academic's preference form responses.
+     * @param timeAvailable [number] Amount of time the academic is available for
+     * @param timeRequired [number] Required amount of time for the unit
+     * @returns Score: number
+     */
+    public scoreAllocation(allocationScores: formResponses, timeAvailable: number, timeRequired: number): number {
 
         // This multiplier will increase the total score if the person has more time available than the class requires, 
         // and reduce it if they have less according to the magnitude of the difference.
-         const timeFitRatio: number = this.timeAvailable / this.timeRequired;
+         const timeFitRatio: number = timeAvailable / timeRequired;
 
         // Return the weighted sum of the factors scaled according to how well the academic's availability matches the class's requirements.
         return ((this.willingWeight * allocationScores.willingness) + (this.expertiseWeight * allocationScores.expertise) 
